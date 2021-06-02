dd7d3d1a7bd0e112a0fd19119ad64f68d54524c9
John Lima <ti.junior@gmail.com>
2021-06-02 08:46:05 -0300

    activity

---
+++ app/src/main/java/br/com/multsoftware/v1/appnews/ui/FavoriteActivity.kt
@@ -0,0 +1,13 @@
+package br.com.multsoftware.v1.appnews.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.multsoftware.v1.appnews.R
+
+class FavoriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
