874e47ba959eae9b7da7c5ee09d6ba7678e0a0e2
John Lima <ti.junior@gmail.com>
2021-06-07 23:04:34 -0300

    upgrade 06

--- app/src/main/java/br/com/multsoftware/v1/appnews/view/ArticleActivity.kt
+++ app/src/main/java/br/com/multsoftware/v1/appnews/view/ArticleActivity.kt
@@ -3,6 +3,8 @@ package br.com.multsoftware.v1.appnews.view
 import android.webkit.WebViewClient
 import br.com.multsoftware.v1.appnews.R
 import br.com.multsoftware.v1.appnews.model.Article
+import br.com.multsoftware.v1.appnews.model.data.NewsDataSource
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_article.*
 
 class ArticleActivity : AbstractActivity() {
@@ -13,12 +15,21 @@ class ArticleActivity : AbstractActivity() {
 
     override fun onInject() {
         getArticle()
+        val dataSource = NewsDataSource(this)
 
         webView.apply {
             webViewClient = WebViewClient()
             article.url?.let { url ->
                 loadUrl(url)
             }
+        }
+
+        fab.setOnClickListener {
+            dataSource.saveArticle(article)
+            Snackbar.make(
+                it, R.string.article_saved_successful,
+                Snackbar.LENGTH_LONG
+            ).show()
         }
     }
 
