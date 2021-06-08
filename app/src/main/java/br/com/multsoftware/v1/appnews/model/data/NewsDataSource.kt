50f6d0f310a62260adf9601ee1679f6d91453974
John Lima <ti.junior@gmail.com>
2021-06-07 22:56:41 -0300

    upgrade 06

--- app/src/main/java/br/com/multsoftware/v1/appnews/model/data/NewsDataSource.kt
+++ app/src/main/java/br/com/multsoftware/v1/appnews/model/data/NewsDataSource.kt
@@ -1,23 +1,28 @@
 package br.com.multsoftware.v1.appnews.model.data
 
+import android.content.Context
+import br.com.multsoftware.v1.appnews.model.Article
+import br.com.multsoftware.v1.appnews.model.db.ArticleDatabase
 import br.com.multsoftware.v1.appnews.network.RetrofitInstance
+import br.com.multsoftware.v1.appnews.presenter.favorite.FavoriteHome
 import br.com.multsoftware.v1.appnews.presenter.news.NewsHome
 import br.com.multsoftware.v1.appnews.presenter.search.SearchHome
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 /**
  * Created by João Bosco on 05/06/2021.
  * e-mail - Support: ti.junior@gmail.com
  */
-class NewsDataSource {
+class NewsDataSource(context: Context) {
+
+    private var db: ArticleDatabase = ArticleDatabase(context)
+    private var newsRepository: NewsRepository = NewsRepository(db)
 
     fun getBreakingNews(callback: NewsHome.Presenter) {
         GlobalScope.launch(Dispatchers.Main) {
             val response = RetrofitInstance.api.getBreakingNews("br")
             if (response.isSuccessful) {
-                response.body()?.let {newsResponse ->
+                response.body()?.let { newsResponse ->
                     callback.onSuccess(newsResponse)
                 }
                 callback.onComplete()
@@ -39,6 +44,31 @@ class NewsDataSource {
             } else {
                 callback.onError(response.message())
                 callback.onComplete()
+            }
+        }
+    }
+
+    fun saveArticle(article: Article) {
+        GlobalScope.launch(Dispatchers.Main) {
+            newsRepository.updateInsert(article)
+        }
+    }
+
+    fun getAllArticle(callback: FavoriteHome) {
+        var allArticles: List<Article>
+        CoroutineScope(Dispatchers.IO).launch {
+            allArticles = newsRepository.getAll()
+
+            withContext(Dispatchers.Main) {
+                callback.showArticles(allArticles)
+            }
+        }
+    }
+
+    fun deleteArticle(article: Article?) {
+        GlobalScope.launch(Dispatchers.Main) {
+            article?.let { articleSafe ->
+                newsRepository.delete(articleSafe)
             }
         }
     }
